16ec42bf139c21ae543690d17160b7cab375129e
MarcoAntonioBarrientosRuano <160303634+MarcoAntonioBarrientosRuano@users.noreply.github.com>
2024-06-08 10:52:38 -0600

    Add files via upload

---
+++ Mantenimiento/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MantenimientoDeProductos
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtExistencia = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnRecuperar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(460, 150);
+            this.dataGridView1.TabIndex = 0;
+
+            this.txtCodigo.Location = new System.Drawing.Point(12, 180);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+
+            this.txtNombre.Location = new System.Drawing.Point(118, 180);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+
+            this.txtPrecio.Location = new System.Drawing.Point(224, 180);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 3;
+
+            this.txtExistencia.Location = new System.Drawing.Point(330, 180);
+            this.txtExistencia.Name = "txtExistencia";
+            this.txtExistencia.Size = new System.Drawing.Size(100, 20);
+            this.txtExistencia.TabIndex = 4;
+
+            this.btnGuardar.Location = new System.Drawing.Point(12, 210);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+
+            this.btnEditar.Location = new System.Drawing.Point(93, 210);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+
+            this.btnBorrar.Location = new System.Drawing.Point(174, 210);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 7;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+
+            this.btnRecuperar.Location = new System.Drawing.Point(255, 210);
+            this.btnRecuperar.Name = "btnRecuperar";
+            this.btnRecuperar.Size = new System.Drawing.Size(75);
+
---
+++ Mantenimiento/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MantenimientoDeProductos
+{
+    public partial class Form1 : Form
+    {
+        private List<Producto>
+            productos = new List<Producto>
+                ();
+        private string connectionString = "tu_cadena_de_conexion_aqui"; // Coloca aquí tu cadena de conexión a la base de datos
+        private readonly object dataGridView1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarUsuarios();
+            ActualizarGrid();
+        }
+
+        private void CargarUsuarios()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Usuario, Nombre FROM Usuarios";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cmbUsuarios.Items.Add(new Usuario
+                    {
+                        UsuarioID = reader["Usuario"].ToString(),
+                        Nombre = reader["Nombre"].ToString()
+                    });
+                }
+                conn.Close();
+            }
+            cmbUsuarios.DisplayMember = "Nombre";
+            cmbUsuarios.ValueMember = "UsuarioID";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto
+            {
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Precio = decimal.Parse(txtPrecio.Text),
+                Existencia = int.Parse(txtExistencia.Text),
+                UsuarioID = ((Usuario)cmbUsuarios.SelectedItem).UsuarioID
+            };
+
+            productos.Add(producto);
+            ActualizarGrid();
+            LimpiarCampos();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var codigo = dataGridView1.CurrentRow.Cells["Codigo"].Value.ToString();
+                var producto = productos.Find(p => p.Codigo == codigo);
+                if (producto != null)
+                {
+                    producto.Nombre = txtNombre.Text;
+                    producto.Precio = decimal.Parse(txtPrecio.Text);
+                    producto.Existencia = int.Parse(txtExistencia.Text);
+                    producto.UsuarioID = ((Usuario)cmbUsuarios.SelectedItem).UsuarioID;
+                    Actualizargrid();
+                    Limpiarcampos(): 
+		}
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var codigo = dataGridView1.CurrentRow.Cells["Codigo"].Value.ToString();
+                var producto = productos.Find(p => p.Codigo == codigo);
+                if (producto != null)
+                {
+                    productos.Remove(producto);
+                    ActualizarGrid();
+                    LimpiarCampos();
+                }
+            }
+        }
+
+        private void btnRecuperar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var codigo = dataGridView1.CurrentRow.Cells["Codigo"].Value.ToString();
+                var producto = productos.Find(p => p.Codigo == codigo);
+                if (producto != null)
+                {
+                    txtCodigo.Text = producto.Codigo;
+                    txtNombre.Text = producto.Nombre;
+                    txtPrecio.Text = producto.Precio.ToString();
+                    txtExistencia.Text = producto.Existencia.ToString();
+                    cmbUsuarios.SelectedValue = producto.UsuarioID;
+                }
+            }
+        }
+
+        private void ActualizarGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = productos;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+            txtExistencia.Clear();
+            cmbUsuarios.SelectedIndex = -1;
+        }
+    }
+
+    public class Producto
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Existencia { get; set; }
+        public string UsuarioID { get; set; }
+
+        public string Usuario => $"{UsuarioID} - {Nombre}";
+    }
+
+    public class Usuario
+    {
+        public string UsuarioID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
+
+		using System;
+		using System.Collections.Generic;
+		using System.Data.SqlClient;
+		using System.Windows.Forms;
+
+		namespace MantenimientoDeProductos
+{
+    public partial class Form1 : Form
+    {
+        private List<Producto>
+            productos = new List<Producto>();
+        private string connectionString = "tu_cadena_de_conexion_aqui"; // Coloca aquí tu cadena de conexión a la base de datos
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarUsuarios();
+            ActualizarGrid();
+        }
+
+        private void CargarUsuarios()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Usuario, Nombre FROM Usuarios";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cmbUsuarios.Items.Add(new Usuario
+                    {
+                        UsuarioID = reader["Usuario"].ToString(),
+                        Nombre = reader["Nombre"].ToString()
+                    });
+                }
+                conn.Close();
+            }
+            cmbUsuarios.DisplayMember = "Nombre";
+            cmbUsuarios.ValueMember = "UsuarioID";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto
+            {
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Precio = decimal.Parse(txtPrecio.Text),
+                Existencia = int.Parse(txtExistencia.Text),
+                UsuarioID = ((Usuario)cmbUsuarios.SelectedItem).UsuarioID
+            };
+
+            productos.Add(producto);
+            ActualizarGrid();
+            LimpiarCampos();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var codigo = dataGridView1.CurrentRow.Cells["Codigo"].Value.ToString();
+                var producto = productos.Find(p => p.Codigo == codigo);
+                if (producto != null)
+                {
+                    producto.Nombre = txtNombre.Text;
+                    producto.Precio = decimal.Parse(txtPrecio.Text);
+                    producto.Existencia = int.Parse(txtExistencia.Text);
+                    producto.UsuarioID = ((Usuario)cmbUsuarios.SelectedItem).UsuarioID;
+                    ActualizarGrid();
+                    LimpiarCampos();
+                }
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var codigo = dataGridView1.CurrentRow.Cells["Codigo"].Value.ToString();
+                var producto = productos.Find(p => p.Codigo == codigo);
+                if (producto != null)
+                {
+                    productos.Remove(producto);
+                    ActualizarGrid();
+                    LimpiarCampos();
+                }
+            }
+        }
+
+        private void btnRecuperar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var codigo = dataGridView1.CurrentRow.Cells["Codigo"].Value.ToString();
+                var producto = productos.Find(p => p.Codigo == codigo);
+                if (producto != null)
+                {
+                    txtCodigo.Text = producto.Codigo;
+                    txtNombre.Text = producto.Nombre;
+                    txtPrecio.Text = producto.Precio.ToString();
+                    txtExistencia.Text = producto.Existencia.ToString();
+                    cmbUsuarios.SelectedValue = producto.UsuarioID;
+                }
+            }
+        }
+
+        private void ActualizarGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = productos;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+            txtExistencia.Clear();
+            cmbUsuarios.SelectedIndex = -1;
+        }
+    }
+
+    public class Producto
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Existencia { get; set; }
+        public string UsuarioID { get; set; }
+
+        public string Usuario => $"{UsuarioID} - {Nombre}";
+    }
+
+    public class Usuario
+    {
+        public string UsuarioID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
